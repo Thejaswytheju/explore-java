48966430d8cf5c4ff7b09d01ca8b5e1a360aa270
Jay <jk.questt@gmail.com>
2017-09-14 22:48:51 +0530

    equals and hash code learning

---
+++ src/main/java/com/theja/java/study/explorejava/hashcode/Employee2.java
@@ -0,0 +1,14 @@
+package com.theja.java.study.explorejava.hashcode;
+
+public class Employee2 extends Employee {
+
+    @Override
+    public int hashCode() {
+        return 100;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return true;
+    }
+}
---
+++ src/test/java/com/theja/java/study/explorejava/hashcode/Employee2Test.java
@@ -0,0 +1,26 @@
+package com.theja.java.study.explorejava.hashcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Employee2Test {
+    @Test
+    public void equals() {
+        Employee2 e1=new Employee2();
+        Employee2 e2=new Employee2();
+        boolean results = e1.equals(e2);
+        Assert.assertTrue(results);
+        results = e1.equals(null);
+        Assert.assertTrue(results);
+    }
+
+    @Test
+    public  void hashvalue() {
+        Employee2 e1=new Employee2();
+        Employee2 e2=new Employee2();
+        int e1hashvalue=e1.hashCode();
+        int e2hashvalue=e2.hashCode();
+        Assert.assertEquals(e1hashvalue, e2hashvalue);
+    }
+
+}
